7fd9a4811a8cb46d124241d7c651e5dffdb614d4
Bobbylee Ingalls <ingallsbobbylee@yahoo.com>
2018-11-26 16:43:35 -0600

    updated material UI build bug

--- baap-gatsby/src/pages/home.js
+++ baap-gatsby/src/pages/home.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Card from '@material-ui/core/Card';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
